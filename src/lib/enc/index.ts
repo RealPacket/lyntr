4614abdeedd25cebfcd094c9ee1153df3f15682a
RealPacket <107498533+RealPacket@users.noreply.github.com>
2024-08-03 21:44:16 -0500

    feat(e2e-encryption): add lib/enc/index.ts

---
+++ src/lib/enc/index.ts
@@ -0,0 +1,6 @@
+import dec from './decrypt';
+import enc from './encrypt';
+import generateKeyPair from './generateKeyPair';
+import deriveKey from './deriveKey';
+export default enc;
+export { enc, dec, generateKeyPair, deriveKey };
